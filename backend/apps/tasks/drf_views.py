737723c89a6142c1db1e5c5a8e2e097602d92b70
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-21 21:57:34 +0800

    Create drf_views.py

---
+++ backend/apps/tasks/drf_views.py
@@ -0,0 +1,9 @@
+from rest_framework import decorators, views, mixins, generics, viewsets
+
+from . import models, serializers
+
+generics.GenericAPIView
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = models.Task.objects.all()
+    serializer_class = serializers.TaskSerializer
