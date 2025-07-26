56dbc97be7e388851ff4a26b99bd5f7a793fef5f
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-26 19:08:47 +0800

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,21 +17,34 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
+from rest_framework.authtoken.views import obtain_auth_token
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from apps.core import register_signals
+
+register_signals()  # 像这种预定义导入暂且放在此处，按照我的理解 urls.py 放这些不太合适
 
 urlpatterns = [
     # django admin
     path('admin/', admin.site.urls),
-    # drf_spectacular
+    # drf_spectacular 交互式文档
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='swagger-ui'),
-    # drf
-    path('api-auth/', include('rest_framework.urls')),  # 用户登录页面
+    # drf 用户登录页面
+    path('api-auth/', include('rest_framework.urls')),
+    # drf 获取 Token
+    path('api-token-auth/', obtain_auth_token),
+    # rest_framework_simplejwt 获取和刷新 Token
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path("blog/", include("apps.blog.urls")),
-    path("tasks/", include("apps.tasks.urls")),
-    path("invitationcode/", include("apps.invitationcode.urls")),
-    path("auth2/", include("apps.auth2.urls")),
+    path("v1/blog/", include("apps.blog.urls")),
+    path("v1/tasks/", include("apps.tasks.urls")),
+    path("v1/invitationcode/", include("apps.invitationcode.urls")),
+    path("v1/auth2/", include("apps.auth2.urls")),
+
+    path("v1/ninja/api/", include("apps.api.urls")),
 ]
 
 # 【知识点】django-debug-toolbar
