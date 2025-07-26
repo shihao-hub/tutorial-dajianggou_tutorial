79c7d727b64e70395f4bef8178eb064a97edba1f
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-26 19:03:57 +0800

    Update models.py

--- backend/apps/auth2/models.py
+++ backend/apps/auth2/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
-class User(models.Model):
-    username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+class MyUser(models.Model):
+    # max_length 主要指定数据库字段的最大长度，并不属于校验，真正的校验交给 drf Serializer
+    username = models.CharField(verbose_name="用户名", max_length=255, unique=True)
+    password = models.CharField(verbose_name="密码", max_length=255)
+    is_active = models.BooleanField(verbose_name="是否激活", default=True)
