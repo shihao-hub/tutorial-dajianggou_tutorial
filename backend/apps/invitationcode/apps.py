fe6788c840a0d57f5e8874edd2e79bf5d4b6203b
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-07-28 22:51:37 +0800

    Update and rename apps.ppy to apps.py

--- backend/apps/invitationcode/apps.py
+++ backend/apps/invitationcode/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class InvitationcodeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.invitationcode'
+    verbose_name = "邀请码"
