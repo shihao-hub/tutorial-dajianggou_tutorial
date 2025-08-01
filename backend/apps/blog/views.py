20c79de118fde03f4f59c81e4ea36015ade40555
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-08-01 16:38:34 +0800

    Update views.py

--- backend/apps/blog/views.py
+++ backend/apps/blog/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 
-from ..core import exceptions
 
-
-def test_index(request: HttpRequest):
-    if request.method not in ["GET"]:
-        raise exceptions.NotAllowedMethodError(request.method)
-    return HttpResponse(b"<body>blog:test_index</body>")
