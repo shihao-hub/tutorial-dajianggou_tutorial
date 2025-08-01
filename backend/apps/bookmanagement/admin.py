b5a22f8105699306b0acbdba6b6e3aca1b05b7d9
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2025-08-01 16:38:48 +0800

    Update admin.py

--- backend/apps/bookmanagement/admin.py
+++ backend/apps/bookmanagement/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Book)  # 使用 register 的装饰器
 class BookAdmin(admin.ModelAdmin):
     pass
 
-
-admin.site.register(models.Book, BookAdmin)
+# admin.site.register(models.Book, BookAdmin) #  使用 register 的方法
